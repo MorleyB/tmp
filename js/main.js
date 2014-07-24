9b600f33b44830e0cf98c3955ca98d48583049eb
partiallypro <josh@virtualblend.com>
2014-07-24 13:27:53 -0500

    from L.A to Tokyo

--- js/main.js
+++ js/main.js
@@ -2,7 +2,10 @@
 // For extensive details about creating a Populr theme, please visit
 // http://developers.populr.me/theming
 */
-
+    $("slide a").addClass("fancybox");
+	$(document).ready(function() {
+		$(".fancybox").fancybox();
+	});
 
 // $(document).on('pop-initialized', function(){
 //
