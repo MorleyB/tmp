ee8de249298dba3a3635fa411b630fc9534ded3f
partiallypro <josh@virtualblend.com>
2014-07-24 13:48:58 -0500

    Comitted

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@
 // For extensive details about creating a Populr theme, please visit
 // http://developers.populr.me/theming
 */
-$( "slide a" ).addClass( "fancybox" );
+$( ".slide a" ).addClass( "fancybox" );
+$("slide a[href^='http']").attr('target','');
 $(document).ready(function() {
 	$(".fancybox").fancybox();
 	});
